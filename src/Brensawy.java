4d513798dd9a7f0eccec75a643105b16b9b6f8fe
Mohamed Raafat <mohamedra2fat@gmail.com>
2014-04-11 13:36:56 +0200

    Update Brensawy.java
    Started working on parseMethod, still not finished yet

--- src/Brensawy.java
+++ src/Brensawy.java
@@ -10,66 +10,70 @@ public class Brensawy {
 	static BufferedReader bf = null;
 	static Stack<String> curleyStack = null;
 	static ArrayList<String> currentBlock = null;
-	public static void main(String [] death) throws IOException{
+
+	public static void main(String[] death) throws IOException {
 		bf = new BufferedReader(new FileReader(new File("Code01out.java")));
 		curleyStack = new Stack<String>();
 		currentBlock = new ArrayList<String>();
 		parseClass();
-//		while(bf.ready()){
-//			String token = bf.readLine();
-//			String [] splitted = token.split("\t");
-//			
-//		}
+		// while(bf.ready()){
+		// String token = bf.readLine();
+		// String [] splitted = token.split("\t");
+		//
+		// }
 	}
-	public static boolean parseClass() throws IOException{
-		String [] classDef = {"CS", "ID", "LC"};
-		for(int i=0; i < classDef.length; i++){
+
+	public static boolean parseClass() throws IOException {
+		String[] classDef = { "CS", "ID", "LC" };
+		for (int i = 0; i < classDef.length; i++) {
 			String token = getToken();
-			if(!token.equals(classDef[i]))
+			if (!token.equals(classDef[i]))
 				return false;
 		}
 		curleyHandle(1);
-		if(!parseClassBlock()){
+		if (!parseClassBlock()) {
 			return false;
 		}
-		if(curleyStack.empty())
+		if (curleyStack.empty())
 			return true;
 		return false;
 	}
-	
-	public static boolean parseClassBlock(){
-		while(true){
+
+	public static boolean parseClassBlock() {
+		while (true) {
 			String token = getToken();
-			if(token.equals("LC"))
+			if (token.equals("LC"))
 				curleyHandle(1);
-			else if(token.equals("RC"))
+			else if (token.equals("RC"))
 				curleyHandle(2);
-			if(curleyStack.empty())
-				if(!getToken().equals("")){
+			if (curleyStack.empty())
+				if (!getToken().equals("")) {
 					return false;
-				}else{				
+				} else {
 					return handleClassBlock();
 					break;
 				}
 			currentBlock.add(token);
 		}
 	}
-	public static boolean curleyHandle(int x){
+
+	public static boolean curleyHandle(int x) {
 		// 1 for { LC
 		// 2 for } RC
-		if(x == 1){
+		if (x == 1) {
 			curleyStack.push("LC");
-		}else{
-			if(curleyStack.peek() == "LC")
+		} else {
+			if (curleyStack.peek() == "LC")
 				curleyStack.pop();
-			else if(curleyStack.empty())
+			else if (curleyStack.empty())
 				return false;
 			else
 				curleyStack.push("RC");
 		}
 		return true;
 	}
-	public static String getToken(){
+
+	public static String getToken() {
 		try {
 			return bf.readLine().split("\t")[0];
 		} catch (IOException e) {
@@ -77,5 +81,48 @@ public class Brensawy {
 			e.printStackTrace();
 		}
 		return "";
+	}
+
+	public static boolean parseMethodHead() {
+		if (getToken().equals("AC")) {
+			if (getToken().equals("ST")) {
+				if (getToken().equals("RT")) {
+					return true;
+				}
+			}
+		}
+		return false;
+
+	}
+
+	public static boolean parseMethodInput() {
+		String token = getToken();
+		boolean result;
+		boolean firstID = true;
+		boolean lastChComma = false;
+		if (token.equals("LB")) {
+			token = getToken();
+			while (true) {
+				if (token.equals("RB"))
+					break;
+				else {
+					if (token.equals("DT")) {
+						token = getToken();
+						if (token.equals("ID")) {
+							token = getToken();
+							if (token.equals("FA")) {
+								token = getToken();
+								lastChComma = true;
+							}else //The next token is not comma, however the p
+								result = true;
+						}else
+							result = false;
+					}else
+						result = false;
+
+				}
+			}
+		}
+
 	}
 }
